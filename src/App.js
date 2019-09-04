19fba6560a69c04afd4fe8c35f149261eec40ff2
Ilmari <ilmari.koskinen1@gmail.com>
2019-09-04 13:49:06 +0300

    Make button click to add an item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const handleClick = () => {
+    setItems([...items, 'something']);
+  };
+
   return (
     <div>
       <h1 data-title-e2e-test>Todo app</h1>
       <input data-new-item-text-field-e2e-test />
-      <button data-create-new-item-button-e2e-test>Add</button>
-      <ul data-todo-list-e2e-test></ul>
+      <button
+        data-create-new-item-button-e2e-test
+        data-create-new-item-button-unit-test
+        onClick={handleClick}
+      >
+        Add
+      </button>
+      <ul data-todo-list-e2e-test data-todo-list-unit-test>
+        {items.map(item => (
+          <li key={item} data-todo-item-unit-test></li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import TestRenderer, { act } from 'react-test-renderer';
 import App from './App';
 
-it('renders correctly', () => {
-  const output = renderer.create(<App />).toJSON();
-  expect(output).toMatchSnapshot();
+describe('App', () => {
+  let renderer;
+  let instance;
+
+  beforeEach(() => {
+    renderer = TestRenderer.create(<App />);
+    instance = renderer.root;
+  });
+
+  it('renders correctly', () => {
+    const output = renderer.toJSON();
+
+    expect(output).toMatchSnapshot();
+  });
+
+  describe('given create new item button is clicked', () => {
+    let todoList;
+    let item;
+
+    beforeEach(() => {
+      todoList = instance.find(e => e.props['data-todo-list-unit-test']);
+      const createNewItemButton = instance.find(
+        e => e.props['data-create-new-item-button-unit-test']
+      );
+
+      act(() => {
+        createNewItemButton.props.onClick();
+      });
+
+      item = instance.find(e => e.props['data-todo-item-unit-test']);
+    });
+
+    it('new item is added to items list', () => {
+      expect(item).toBeDefined();
+    });
+  });
 });
